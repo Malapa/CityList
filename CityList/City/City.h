6c90f74dc48148f7970f1fec66f85028f9c5a37f
Malapa <malapa@126.com>
2015-07-29 09:38:53 +0800

    first

---
+++ CityList/City/City.h
@@ -0,0 +1,14 @@
+//
+//  City.h
+//  CityList
+//
+//  Created by bhb on 15/7/29.
+//  Copyright (c) 2015年 Micky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface City : NSObject
+@property (nonatomic, copy) NSString *cityName; // 城市名
+@property (nonatomic, copy) NSString *cityLetter; // 城市名拼音
+@end
